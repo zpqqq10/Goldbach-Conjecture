623889bfa696299ca14da863ba20ba1f84e1724e
AI哀 <101961583+HaibaraAIr@users.noreply.github.com>
2022-06-07 16:51:04 +0800

    Add files via upload

---
+++ PhraseQuery.py
@@ -0,0 +1,83 @@
+from InvertedIndex import build_Iindex
+import utils
+def handler(query):
+    word_list = word_split(query)
+    result = phrasequery(query)
+    utils.print_result(word_list, result, 'Phrase Query')
+
+# 'of': {1: [60, 69, 144, 157, 178, 191, 209, 274], 6: [12, 22]}
+def getinverted_index():
+    inv,_ = build_Iindex()
+    return inv
+
+def word_split(text):
+    word_list = []
+    wcurrent = []
+    windex = 0
+
+    for i, c in enumerate(text):
+        if c.isalnum():
+            wcurrent.append(c)
+        elif wcurrent:
+            windex = windex + 1
+            word = u''.join(wcurrent).lower()
+            word_list.append(word)
+            wcurrent = []
+ 
+    if wcurrent:
+        windex = windex + 1
+        word = u''.join(wcurrent).lower()
+        word_list.append(word)
+ 
+    return word_list
+
+def phrasequery(query):
+    inverted = getinverted_index()
+    words = [word for word in word_split(query) if word in list(inverted.keys())]
+
+    # print(words)
+
+    tempDic = {}
+    doc_return = []
+
+    for word in words:
+        word_doc_ids=inverted[word].keys()
+        tempDic.setdefault(word,{})
+        for ID in word_doc_ids:
+            word_doc_position =  inverted[word][ID]
+            tempDic[word].setdefault(ID,word_doc_position)
+
+    # print(tempDic)
+
+    if len(words)>1:
+        minKey = {}
+        for i in range(0,len(words)):
+            tempKeys = tempDic[words[i]].keys()
+            minKey.setdefault(i,tempKeys)
+        minKeyNew = minKey[0]
+        for i in range(1,len(words)):
+            minKeyNew = [val for val in minKeyNew if val in minKey[i]]
+        for key in minKeyNew:
+            list1 = tempDic[words[0]][key]
+            isAdd = []
+            for position in list1:
+                isAddForEach = []
+                for j in range(1,len(words)):
+                    if position+j not in tempDic[words[j]][key]:
+                        isAddForEach.append(0)
+                    else:
+                        isAddForEach.append(1)
+                if 0 in isAddForEach:
+                    isAdd.append(0)
+                else:
+                    isAdd.append(1)
+            if 1 in isAdd:
+                doc_return.append(key)
+    elif len(words)==1:
+        key = list(tempDic[words[0]].keys())[0]
+        doc_return.append(key)
+    results = []
+    for doc_id in doc_return:
+        if doc_id not in results:
+            results.append(doc_id)
+    return results
